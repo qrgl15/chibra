5d9c1f49a2510b419e8d1e197ad0a719846318d2
Otaki <a1a1a2a4@gmail.com>
2017-03-21 09:27:10 +0000

    update

--- js/top.js
+++ js/top.js
@@ -1,66 +1,3 @@
-/* video position */
-$(function() {
-  var mv = $('.p_top-video');
-  var vw = 1280;
-  var vh = 720;
-  var sf = vw / vh;
-
-  function videoWrap() {
-    var wh = $(window).height();
-    var hh = $('.p_top-header-wrap').height();
-
-    mv.height(wh - hh);
-  }
-
-  function videoPos() {
-    var ww = $(window).width();
-    var th = ww / sf;
-
-    if (th < mv.height()) {
-      th = mv.height();
-      ww = th * sf;
-    }
-
-    $('.p_top-video > video').css({
-      top: (mv.height() - th) / 2,
-      left: (mv.width() - ww) / 2,
-      width: ww,
-      height: th
-    });
-  }
-  videoWrap();
-  videoPos();
-
-  $(window).on(resizeEvent, function() {
-    videoWrap();
-    videoPos();
-  });
-});
-
-/* slideshow */
-$(function() {
-  var $interval = 3000;
-  var $fade_speed = 1000;
-
-  $('.p_top-video-sp__item').css({
-    "position": "relative",
-    "overflow": "hidden"
-  });
-
-  $('.p_top-video-sp__item').hide().css({
-    "position": "absolute"
-  });
-
-  $('.p_top-video-sp__item:first').addClass("is_active").show();
-
-  setInterval(function() {
-    var $active = $('.p_top-video-sp__item.is_active');
-    var $next = $active.next('div').length ? $active.next('div') : $('.p_top-video-sp__item:first');
-    $active.fadeOut($fade_speed).removeClass('is_active');
-    $next.fadeIn($fade_speed).addClass('is_active');
-  }, $interval);
-});
-
 /* scroll button / badge */
 $(function() {
   $('.p_top-mv__scroll').fadeIn('fast');
@@ -124,26 +61,5 @@ $(function() {
         $('.p_top-video-wrap').height(h - pcHeader);
       }
     }, 0);
-  });
-});
-
-/* people slider */
-$(function() {
-  $('.js_top-people-slider').slick({
-    autoplay: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    speed: 300,
-    infinite: true,
-    appendArrows: $('.p_top-people-slider__arrows'),
-    responsive: [{
-      breakpoint: 767,
-      settings: {
-        centerMode: true,
-        centerPadding: '84px',
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }]
   });
 });
